d175b04cb1779ad209e12b2916940eb9bbfc2051
Geert van Horrik <geert@catenalogic.com>
2025-12-19 14:44:09 +0100

    Prepare solution and projects

---
+++ src/Catel.SourceGenerators.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+// All other assembly info is defined in SharedAssembly.cs
+[assembly: AssemblyTitle("Catel.SourceGenerators.Tests")]
+[assembly: AssemblyProduct("Catel.SourceGenerators.Tests")]
+[assembly: AssemblyDescription("Catel SourceGenerators tests library")]
