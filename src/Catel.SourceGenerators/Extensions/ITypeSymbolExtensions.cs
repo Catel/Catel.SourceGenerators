59e76f53fb20d0c7908657c4f03112b2bc80b793
Geert van Horrik <geert@catenalogic.com>
2025-12-23 00:03:31 +0100

    Add IndentedStringBuilder for easier building of generated code

---
+++ src/Catel.SourceGenerators/Extensions/ITypeSymbolExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Catel.SourceGenerators
+{
+    using Microsoft.CodeAnalysis;
+
+    internal static class ITypeSymbolExtensions
+    {
+        public static bool ImplementsInterface(this ITypeSymbol typeSymbol, string interfaceMetadataName)
+        {
+            foreach (var interfaceType in typeSymbol.AllInterfaces)
+            {
+                if (interfaceType.ToDisplayString() == interfaceMetadataName)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/Catel.SourceGenerators/Extensions/IndentedStringBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Catel.SourceGenerators
+{
+    using System.CodeDom.Compiler;
+
+    internal static class IndentedStringBuilderExtensions
+    {
+        internal static readonly string GeneratorVersion = typeof(IndentedStringBuilderExtensions).Assembly.GetName().Version.ToString();
+
+        public static void StartBlock(this IndentedStringBuilder writer)
+        {
+            writer.AppendLine("{");
+            writer.IncrementIndent();
+        }
+
+        public static void EndBlock(this IndentedStringBuilder writer)
+        {
+            writer.DecrementIndent();
+            writer.AppendLine("}");
+        }
+
+        public static void AppendGeneratedCodeAttribute(this IndentedStringBuilder writer, string generatorName)
+        {
+            writer.AppendLine($"""[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Catel.{generatorName}", "{GeneratorVersion}")]""");
+        }
+    }
+}
